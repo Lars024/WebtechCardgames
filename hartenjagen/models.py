ce258fa44f7ad7171e5fa7597b3fcfb9fa606337
unknown <s156485@s156485.campus.tue.nl>
2017-01-13 14:27:56 +0100

    fixed inconsistent use of tabs/spaces

--- hartenjagen/models.py
+++ hartenjagen/models.py
@@ -3,15 +3,15 @@ from django.db import models
 
 # Create your models here.
 class Room(models.Model):
-    name = models.TextField()
-    label = models.SlugField(unique=True)
+	name = models.TextField()
+	label = models.SlugField(unique=True)
 
-    def __unicode__(self):
-        return self.label
-	
+	def __unicode__(self):
+		return self.label
+
 	def __str__(self):
 		return self.name
 
 class Player:
-    room = models.ForeignKey(Room, related_name='player')
-    name = models.TextField()
+	room = models.ForeignKey(Room, related_name='player')
+	name = models.TextField()
